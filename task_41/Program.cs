450b129d8f58e1faf97d6175d70bca18a56c9085
sergei sam <ssi1970@mail.ru>
2022-07-15 13:37:01 +0700

    Задача 41

---
+++ task_41/Program.cs
@@ -0,0 +1,31 @@
+﻿
+namespace Homework_6
+{
+    public class task_41
+    //  Пользователь вводит с клавиатуры M чисел. Посчитайте,
+    //  сколько чисел больше 0 ввёл пользователь.
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите желаемое колличество целых чисел(через пробел): ");
+                int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+                int count = 0;
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] > 0) count++;
+                }
+
+                Console.WriteLine($"Кол-во элементов больше 0 = {count}");
+            }
+            catch
+            {
+                Console.WriteLine("Вы ввели некорректные данные, попробуйте еще раз");
+            }
+
+
+        }
+    }
+}
